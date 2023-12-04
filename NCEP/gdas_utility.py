45898d6031a42c2516cf50352d0ac5f766e038c2
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2023-12-04 11:42:51 -0500

    Update gdas_utility.py

--- NCEP/gdas_utility.py
+++ NCEP/gdas_utility.py
@@ -107,11 +107,11 @@ class GFSDataProcessor:
                     'levels': [':(50|100|150|200|250|300|400|500|600|700|850|925|1000) mb:'],
                 },
             },
-            '.f001': {
-                ':USWRF:': {
-                    'levels': [':top of atmosphere:'],
-                },
-            },
+            #'.f001': {
+            #    ':USWRF:': {
+            #        'levels': [':top of atmosphere:'],
+            #    },
+            #},
             '.f006': {
                 ':LAND:': {
                     'levels': [':surface:'],

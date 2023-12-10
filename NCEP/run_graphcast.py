763ce749d0f850db6f22de9354c5ee30c2da8f0a
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2023-12-10 13:56:39 -0500

    build graphcast model (jax)

--- NCEP/run_graphcast.py
+++ NCEP/run_graphcast.py
@@ -51,3 +51,45 @@ with open("stats/mean_by_level.nc", "rb") as f:
 with open("stats/stddev_by_level.nc","rb") as f:
     stddev_by_level = xarray.load_dataset(f).compute()
     
+# Build jitted functions, and initialize weights
+def construct_wrapped_graphcast(model_config: graphcast.ModelConfig, task_config: graphcast.TaskConfig):
+    """Constructs and wraps the GraphCast Predictor."""
+    # Deeper one-step predictor.
+    predictor = graphcast.GraphCast(model_config, task_config)
+
+    # Modify inputs/outputs to `graphcast.GraphCast` to handle conversion to
+    # from/to float32 to/from BFloat16.
+    predictor = casting.Bfloat16Cast(predictor)
+
+    # Modify inputs/outputs to `casting.Bfloat16Cast` so the casting to/from
+    # BFloat16 happens after applying normalization to the inputs/targets.
+    predictor = normalization.InputsAndResiduals(predictor, diffs_stddev_by_level=diffs_stddev_by_level, mean_by_level=mean_by_level, stddev_by_level=stddev_by_level)
+
+    # Wraps everything so the one-step model can produce trajectories.
+    predictor = autoregressive.Predictor(predictor, gradient_checkpointing=True)
+    return predictor
+
+
+@hk.transform_with_state
+def run_forward(model_config, task_config, inputs, targets_template, forcings):
+    predictor = construct_wrapped_graphcast(model_config, task_config)
+    return predictor(inputs, targets_template=targets_template, forcings=forcings)
+
+# Jax doesn't seem to like passing configs as args through the jit. Passing it
+# in via partial (instead of capture by closure) forces jax to invalidate the
+# jit cache if you change configs.
+def with_configs(fn):
+    return functools.partial(fn, model_config=model_config, task_config=task_config)
+
+# Always pass params and state, so the usage below are simpler
+def with_params(fn):
+    return functools.partial(fn, params=params, state=state)
+
+# Our models aren't stateful, so the state is always empty, so just return the
+# predictions. This is requiredy by our rollout code, and generally simpler.
+def drop_state(fn):
+    return lambda **kw: fn(**kw)[0]
+
+init_jitted = jax.jit(with_configs(run_forward.init))
+
+run_forward_jitted = drop_state(with_params(jax.jit(with_configs(run_forward.apply))))

c1e0f38d8a1f6d75b7a21187550934a936d52d5e
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2023-12-10 19:25:19 -0500

    job card

--- NCEP/graphcast_job.sh
+++ NCEP/graphcast_job.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=$(nproc)  # Use all available CPU cores
+#SBATCH --time=1:30:00  # Adjust this to your estimated run time
+#SBATCH --job-name=graphcast
+#SBATCH --output=graphcast_job_output.txt
+#SBATCH --error=graphcast_job_error.txt

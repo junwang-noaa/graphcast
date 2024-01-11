d5c5b54e7a3af993bcc15e340e03688d3e45d8d7
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2024-01-11 12:35:38 -0500

    Update gdas_utility.py

--- NCEP/gdas_utility.py
+++ NCEP/gdas_utility.py
@@ -4,7 +4,7 @@ Description
 Revision history:
     -20231010: Sadegh Tabas, initial code
     -20231204: Sadegh Tabas, calculating toa incident solar radiation, parallelizing, updating units, and resolving memory issues
-    -20240115: Sadegh Tabas, (i)removing Pysolar as tisr would be calc through GC, (ii) add NOMADS option for downloading data, (iii) add 37 pressure levels, (iv) configurations for hera
+    -20240112: Sadegh Tabas, (i)removing Pysolar as tisr would be calc through GC, (ii) add NOMADS option for downloading data, (iii) add 37 pressure levels, (iv) configurations for hera
 '''
 import os
 import sys

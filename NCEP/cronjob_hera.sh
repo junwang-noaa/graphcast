20a939c5d87d68725b66c0be944bde22a2aa95d0
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2024-01-20 13:58:49 -0500

    Update cronjob_hera.sh

--- NCEP/cronjob_hera.sh
+++ NCEP/cronjob_hera.sh
@@ -1,4 +1,19 @@
 #!/bin/bash --login
 sh /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job1_hera.sh
+echo "Job 1 is running"
+sleep 60  # Simulating some work
+echo "Job 1 completed"
+
 sbatch /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job2_hera.sh
+job2_id=$(sbatch /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job2_hera.sh | awk '{print $4}')
+echo "Job 2 is running"
+sleep 5  # Simulating some work
+echo "Job 2 completed"
+
+# Wait for job 2 to complete
+wait $job2_id
+
 sh /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job3_hera.sh
+echo "Job 3 is running"
+sleep 5  # Simulating some work
+echo "Job 3 completed"

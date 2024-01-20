dad939b00522620bacfc2255e4663715f5e40d9d
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2024-01-19 23:25:23 -0500

    Update cronjob_hera.sh

--- NCEP/cronjob_hera.sh
+++ NCEP/cronjob_hera.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/bash --login
 sh /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job1_hera.sh
 sbatch /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job2_hera.sh
+sh /scratch1/NCEPDEV/nems/AIML/graphcast/NCEP/gc_job3_hera.sh

d8c088df6dd8a14e6e1a98b38baa256b8a02620c
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2024-07-09 20:47:43 -0400

    Update gcjob_13pl_cloud.sh

--- NCEP/gcjob_13pl_cloud.sh
+++ NCEP/gcjob_13pl_cloud.sh
@@ -38,7 +38,7 @@ prev_datetime=$( date -d "$datetime 18 hour ago" "+%Y%m%d%H" )
 echo "Current state: $curr_datetime"
 echo "6 hours earlier state: $prev_datetime"
 
-forecast_length=40
+forecast_length=64
 echo "forecast length: $forecast_length"
 
 num_pressure_levels=13
